afed874e303eaccd24d3214471c81f6225d6dfa7
Akinleyejoshua <akinleyejoshua.dev@gmail.com>
2025-11-27 22:09:48 +0100

    new

--- app/api/sermon/[id]/route.ts
+++ app/api/sermon/[id]/route.ts
@@ -1,21 +1,13 @@
 import SermonService from "../../services/sermon";
 import { NextResponse } from "next/server";
 
-<<<<<<< HEAD
 export async function GET(request: Request, { params }: any) {
-=======
-export async function GET(request: Request, { params }: any ) {
->>>>>>> a6d4456e7540aa6928da19d16a06aa79ce1685df
     const sermonService = new SermonService();
     const result = await sermonService.get_sermon(params.id);
     return NextResponse.json(result);
 }
 
-<<<<<<< HEAD
 export async function PUT(request: Request, { params }: any) {
-=======
-export async function PUT(request: Request, { params }: any ) {
->>>>>>> a6d4456e7540aa6928da19d16a06aa79ce1685df
     const sermonService = new SermonService();
     const data = await request.json();
     const result = await sermonService.update_sermon(params.id, data);
