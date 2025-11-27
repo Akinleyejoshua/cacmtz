878e73568379c37aa9cb81806d3fd737c581a74d
Joshua <akinleyejoshua.dev@gmail.com>
2025-11-27 21:36:08 +0100

    Update route.ts

--- app/api/sermon/[id]/route.ts
+++ app/api/sermon/[id]/route.ts
@@ -7,14 +7,14 @@ export async function GET(request: Request, { params }: any ) {
     return NextResponse.json(result);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, { params }: any ) {
     const sermonService = new SermonService();
     const data = await request.json();
     const result = await sermonService.update_sermon(params.id, data);
     return NextResponse.json(result);
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, { params }: any) {
     const sermonService = new SermonService();
     const result = await sermonService.delete_sermon(params.id);
     return NextResponse.json(result);
