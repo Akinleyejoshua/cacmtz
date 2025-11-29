d26205e0abf1ef7847b8266ad7bb6a7477a47acc
Akinleyejoshua <akinleyejoshua.dev@gmail.com>
2025-11-29 02:13:07 +0100

    new

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@
 import MainHeader from "../components/main-header";
 import { SplashScreen } from "../components/splash-screen";
 import { useChurchProfileManager } from "../hooks/use-church-profile-manager";
+import { useLandingPage } from "../hooks/use-landing-page";
 import Footer from "../sections/footer";
 import ProfileSection from "../sections/profile-section";
 
@@ -10,6 +11,7 @@ import ProfileSection from "../sections/profile-section";
 
 export default function Home() {
   const {profiles} = useChurchProfileManager();
+  const {generalSettings} = useLandingPage();
 
   if (profiles.length == 0) return SplashScreen();
 
@@ -18,7 +20,7 @@ export default function Home() {
     <div>
      <MainHeader />
      <ProfileSection profiles={profiles}/>
-    <Footer />
+    <Footer generalSettings={generalSettings}/>
     </div>
   );
 }
